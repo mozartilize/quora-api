af9c29cb81adc2a2d6ee7286aa53d908fd33fdee
mozart <mozartilize@gmail.com>
2018-03-21 21:26:10 +0700

    [wip] AccountActivationAPI

--- src/quora/blueprints/api/accounts.py
+++ src/quora/blueprints/api/accounts.py
@@ -1,4 +1,7 @@
-from flask import request, abort, url_for
+from datetime import datetime
+import jwt
+import pytz
+from flask import request, abort, url_for, current_app
 from flask_restful import Resource
 from sqlalchemy import select
 from marshmallow.exceptions import ValidationError
@@ -6,6 +9,7 @@ from marshmallow.exceptions import ValidationError
 from quora.services.authentication import (
     auth,
     verify_activation_token,
+    generate_auth_token,
 )
 from quora.tables import db, accounts
 from quora.schemas.account import (
@@ -34,7 +38,7 @@ class AccountAPI(Resource):
 
 
 class AccountActivationAPI(Resource):
-    def get(self, id, token):
+    def get(self, id):
         q = select([accounts.c.id])\
             .where(accounts.c.id == str(id))
         with db.engine.connect() as conn:
@@ -42,7 +46,30 @@ class AccountActivationAPI(Resource):
             if not acc:
                 return abort(404)
             else:
-                pass
+                token = generate_auth_token(acc.id, secs=15*60)
+                return {'token': token}, 200
+
+    def post(self):
+        token = request.json.get('token')
+        if not token:
+            return {'message': 'Missing token'}, 400
+        payload = jwt.decode(token, current_app.config['SECRET_KEY'])
+        try:
+            q = select([accounts.c.id])\
+                .where(accounts.c.id == payload['account_id'])
+        except KeyError:
+            return {'message': 'Invalid token'}, 400
+        with db.engine.connect() as conn:
+            acc = conn.execute(q).fetchone()
+            if acc:
+                stmt = accounts.update()\
+                    .where(accounts.c.id == acc.id)\
+                    .values(activated_at=datetime.now(tz=pytz.utc))
+                conn.execute(stmt)
+                return {}, \
+                    200, \
+                    {'Location': url_for('.accountapi', id=acc.id)}
+
 
 
 class AccountListAPI(Resource):
@@ -54,6 +81,6 @@ class AccountListAPI(Resource):
             uuid = result.inserted_primary_key[0]
             return {'id': uuid}, \
                 201, \
-                {'Location': url_for('api.accountapi', id=uuid)}
+                {'Location': url_for('.accountapi', id=uuid)}
         except ValidationError as e:
             return {'message': '', 'errors': e.messages}, 400
