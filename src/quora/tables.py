0b4a89e6df4c6eb47984cbd8a39a76f645fe2c8a
arshavindn <mozartilize@gmail.com>
2018-05-30 13:39:50 +0700

    remove import tables

--- src/quora/tables.py
+++ src/quora/tables.py
@@ -2,6 +2,3 @@ from flask_sqlalchemy import SQLAlchemy
 from utils.tables.mixins import meta
 
 db = SQLAlchemy(metadata=meta)
-
-from accounts.tables import accounts
-from questions.tables import questions, answers
