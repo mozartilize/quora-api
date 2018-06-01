49ae3753461b03d7a8a8b9eb50a46919fa516d1f
arshavindn <mozartilize@gmail.com>
2018-06-02 00:59:48 +0700

    more work on packing package

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 packages = ["libs.password", "utils",
             # modules
             "accounts", "questions",
-            "migrations",
             # app
             "quora"]
 
@@ -14,7 +13,31 @@ setup(
     author_email="mozartilize@gmail.com",
     description="Quora-liked API",
     url="https://github.com/mozartilize/quora-api",
+    zip_safe=False,
     packages=packages,
+    include_package_data=True,
+    install_requires=[
+        'flask>=1.0,<2',
+        'sqlalchemy>=1.2,<2',
+        'psycopg2-binary>=2.7,<3',
+        'flask-restful>=0.3.6,<1',
+        'flask-migrate>=2.1,<3',
+        'pyjwt>=1.6,<2',
+        'passlib>=1.7,<2',
+        'flask-sqlalchemy>=2.3,<3',
+        'flask-httpauth>=3.2,<4',
+        'marshmallow>=3.0.0b11,<3.1'
+    ],
+    extras_require={
+        'passlib': ['bcrypt'],
+        'dotenv': ['python-dotenv'],
+        'dev': [
+            'pytest',
+            'watchdog',
+            'flask-shell-ipython',
+            'pylint'
+        ]
+    },
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
