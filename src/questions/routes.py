0471b0af8640b607a34393b57c1d3f54b7a55e85
arshavindn <mozartilize@gmail.com>
2018-07-18 14:37:00 +0700

    remove redundant str converter

--- src/questions/routes.py
+++ src/questions/routes.py
@@ -3,7 +3,7 @@ from questions.resources import QuestionListAPI, QuestionAPI, \
 
 routes = [
     (QuestionListAPI, '/questions'),
-    (QuestionAPI, '/questions/<string:id>'),
-    (AnswerListAPI, '/questions/<string:question_id>/answers'),
-    (AnswerAPI, '/questions/<string:question_id>/answers/<string:id>', '/answers/<string:id>'),
+    (QuestionAPI, '/questions/<id>'),
+    (AnswerListAPI, '/questions/<question_id>/answers'),
+    (AnswerAPI, '/questions/<question_id>/answers/<id>', '/answers/<id>'),
 ]
